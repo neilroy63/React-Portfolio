54ae11aba42901bb01246a6e653c0795245133db
Neil <neil.roy63@gmail.com>
2021-07-17 10:54:00 +1000

    feature/added_component_header

---
+++ src/components/header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <h1>Welcome to my page!</h1>
+      {/* <h2>...</h2>
+      <ul>
+        <li>..</li>
+        <li>..</li>
+        <li>..</li>
+      </ul> */}
+    </div>
+  );
+}
+
+export default Header;
